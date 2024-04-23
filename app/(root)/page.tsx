95b9683246bfaf09f295e79fd0895bbff7046ef4
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-23 22:16:04 +0600

    [feat]: show collections item at home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
+import Collections from "@/components/Collections";
+import Image from "next/image";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <Image
+        src="/banner.png"
+        alt="banner"
+        width={2000}
+        height={1000}
+        className="w-screen"
+      />
+      <Collections />
+    </>
+  );
 };
 
 export default HomePage;
---
+++ components/Collections.tsx
@@ -0,0 +1,28 @@
+import { getCollections } from "@/lib/actions";
+import Image from "next/image";
+import Link from "next/link";
+
+const Collections = async () => {
+  const collections = await getCollections();
+
+  return (
+    <div className="flex flex-col items-center gap-10 py-8 px-5">
+      <p className="text-heading1-bold">Collections</p>
+      <div className="flex items-center justify-center gap-8">
+        {collections.map((collection: CollectionType) => (
+          <Link key={collection._id} href={`/collections/${collection._id}`}>
+            <Image
+              src={collection.image}
+              alt={collection.title}
+              width={300}
+              height={200}
+              className="rounded-lg cursor-pointer h-[10rem] object-cover"
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Collections;
