4cc0f5a40ec0f962ca57e40d018b9230ef44c9ff
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-27 15:20:35 +0600

    update route bug

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,27 +1,32 @@
 import User from "@/lib/models/User";
 import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
     const { userId } = auth();
+
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
     }
+
     await connectToDB();
 
     let user = await User.findOne({ clerkId: userId });
 
+    // When the user sign-in for the 1st, immediately we will create a new user for them
     if (!user) {
       user = await User.create({ clerkId: userId });
       await user.save();
     }
 
     return NextResponse.json(user, { status: 200 });
-  } catch (error) {
-    console.log("[user-get]", error);
-    return new NextResponse("Internal server error", { status: 500 });
+  } catch (err) {
+    console.log("[users_GET]", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 };
-export const dynamic = "force-dynamic";
--- app/api/users/wishlist/route.ts
+++ app/api/users/wishlist/route.ts
@@ -40,4 +40,3 @@ export const POST = async (req: NextRequest) => {
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 };
-export const dynamic = "force-dynamic";
