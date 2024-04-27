dda2e91d8bd5cd1974285c72eb685bc8f4ae42a0
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-04-27 17:25:16 +0600

    update route path

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,4 @@
-import User from "@/lib/models/User";
+import User from "../../../lib/models/User";
 import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs";
 
--- app/api/users/wishlist/route.ts
+++ app/api/users/wishlist/route.ts
@@ -1,4 +1,4 @@
-import User from "@/lib/models/User";
+import User from "../../../../lib/models/User";
 import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
